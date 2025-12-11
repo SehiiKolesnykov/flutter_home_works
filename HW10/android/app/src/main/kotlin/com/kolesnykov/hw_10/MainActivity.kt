998ee771008fb1d6866070265a4ba95538022a4f
Serhii Kolesnykov <kvs191214@gmail.com>
2025-12-11 20:47:35 +0200

    add hw 10 (+files)

---
+++ HW10/android/app/src/main/kotlin/com/kolesnykov/hw_10/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.kolesnykov.hw_10
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ HW10/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'core/theme.dart';
+import 'presentation/home/home_screen.dart';
+import 'package:sizer/sizer.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, screenType) {
+        return MaterialApp(
+          title: 'Асинхронні завдання',
+          debugShowCheckedModeBanner: false,
+          theme: appTheme,
+          home: const HomeScreen(),
+        );
+      },
+    );
+  }
+}
